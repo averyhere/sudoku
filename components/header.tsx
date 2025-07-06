f498768f3ad18bb53a938f9fea84a6d900d0b39e
Avery Ondo <hello@averyhere.com>
2025-07-06 12:32:02 -0600

    fix header layout

--- components/header.tsx
+++ components/header.tsx
@@ -5,25 +5,13 @@ import { AveryOndoLogo } from "@/components/logos";
 import { NewGameButton } from "@/components/Sudoku/new-game-button";
 import { ScoreboardButton } from "@/components/Sudoku/scoreboard";
 
-const ExternalLinks = [
-  {
-    label: (
-      <>
-        <Github className="size-6" />
-        <span className="sr-only">GitHub</span>
-      </>
-    ),
-    href: "https://github.com/averyhere/sudoku",
-  },
-];
-
 export function Header() {
   return (
     <header>
       <div className="w-full mx-auto py-4 flex gap-2 justify-between items-center">
         <div className="flex gap-4 items-center">
-          <div className="flex flex-col items-end">
-            <h1 className="flex text-4xl uppercase leading-none">
+          <div className="flex flex-col">
+            <h1 className="flex text-2xl md:text-4xl uppercase leading-none">
               <Link href="/" className="hover:underline">
                 Sudoku
               </Link>
@@ -38,20 +26,22 @@ export function Header() {
               </Link>
             </div>
           </div>
-
-          <NewGameButton variant="link">New Game</NewGameButton>
-          <ScoreboardButton variant="link">Scoreboard</ScoreboardButton>
+          <NewGameButton variant="link" size="xs">
+            New Game
+          </NewGameButton>
+          <ScoreboardButton variant="link" size="xs">
+            Scoreboard
+          </ScoreboardButton>
         </div>
         <div className="flex gap-4 items-center">
-          <ul className="hidden md:flex gap-8">
-            {ExternalLinks.map((item) => (
-              <li key={item.href}>
-                <Link href={item.href} className="flex items-center gap-1">
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <Link
+            className="hidden md:inline"
+            href="https://github.com/averyhere/sudoku"
+            target="_blank"
+          >
+            <Github className="size-6" />
+            <span className="sr-only">GitHub</span>
+          </Link>
           <ThemeToggle />
         </div>
       </div>
